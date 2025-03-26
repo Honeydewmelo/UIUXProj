c722a93347b1ee8ceed07b04ac51dd59de721872
Ehren Geissler <ehren.geissler@lethpolytech.ca>
2025-03-25 18:45:49 -0600

    Fixed Stupid math ;P

--- script.js
+++ script.js
@@ -46,11 +46,11 @@ fetch('./AllWeap.json')
             epiDmg.innerText = weapon.Epic.Damage;
             legDmg.innerText = weapon.Legendary.Damage;
 
-            comDmgDiv.style.width = (weapon.Common.Damage / weapon.HighestCatDmg) + "%";
-            uncDmgDiv.style.width = ((weapon.Uncommon.Damage - weapon.Common.Damage) / weapon.HighestCatDmg) + "%";
-            rarDmgDiv.style.width = ((weapon.Rare.Damage - (weapon.Uncommon.Damage + weapon.Common.Damage)) / weapon.HighestCatDmg) + "%";
-            epiDmgDiv.style.width = ((weapon.Epic.Damage - (weapon.Rare.Damage + weapon.Uncommon.Damage + weapon.Common.Damage)) / weapon.HighestCatDmg) + "%";
-            legDmgDiv.style.width = ((weapon.Legendary.Damage - (weapon.Epic.Damage + weapon.Rare.Damage + weapon.Uncommon.Damage + weapon.Common.Damage)) / weapon.HighestCatDmg) + "%";
+            comDmgDiv.style.width = ((weapon.Common.Damage / weapon.HighestCatDmg) *100) + "%";
+            uncDmgDiv.style.width = (((weapon.Uncommon.Damage - weapon.Common.Damage) / weapon.HighestCatDmg)*100) + "%";
+            rarDmgDiv.style.width = (((weapon.Rare.Damage - weapon.Uncommon.Damage) / weapon.HighestCatDmg)*100) + "%";
+            epiDmgDiv.style.width = (((weapon.Epic.Damage - weapon.Rare.Damage) / weapon.HighestCatDmg)*100) + "%";
+            legDmgDiv.style.width = (((weapon.Legendary.Damage - weapon.Epic.Damage) / weapon.HighestCatDmg)*100) + "%";
 
             comRel.innerText = weapon.Common.Reload;
             uncRel.innerText = weapon.Uncommon.Reload;
@@ -58,17 +58,17 @@ fetch('./AllWeap.json')
             epiRel.innerText = weapon.Epic.Reload;
             legRel.innerText = weapon.Legendary.Reload;
 
-            legRelDiv.style.width = (weapon.Legendary.Reload / weapon.HighestCatRel) + "%";
-            epiRelDiv.style.width = ((weapon.Epic.Reload - weapon.Legendary.Reload) / weapon.HighestCatRel) + "%";
-            rarRelDiv.style.width = ((weapon.Rare.Reload - (weapon.Epic.Reload + weapon.Legendary.Reload)) / weapon.HighestCatRel) + "%";
-            uncRelDiv.style.width = ((weapon.Uncommon.Reload - (weapon.Rare.Reload + weapon.Epic.Reload + weapon.Legendary.Reload)) / weapon.HighestCatRel) + "%";
-            comRelDiv.style.width = ((weapon.Common.Reload - (weapon.Uncommon.Reload + weapon.Rare.Reload + weapon.Epic.Reload + weapon.Legendary.Reload)) / weapon.HighestCatRel) + "%";
+            legRelDiv.style.width = ((weapon.Legendary.Reload / weapon.HighestCatRel)*100) + "%";
+            epiRelDiv.style.width = (((weapon.Epic.Reload - weapon.Legendary.Reload) / weapon.HighestCatRel)*100) + "%";
+            rarRelDiv.style.width = (((weapon.Rare.Reload - weapon.Epic.Reload) / weapon.HighestCatRel)*100) + "%";
+            uncRelDiv.style.width = (((weapon.Uncommon.Reload - weapon.Rare.Reload) / weapon.HighestCatRel)*100) + "%";
+            comRelDiv.style.width = (((weapon.Common.Reload - weapon.Uncommon.Reload) / weapon.HighestCatRel)*100) + "%";
 
             fireRate.innerText = weapon.FireRate;
-            fireRateDiv.style.width = (weapon.FireRate / weapon.HighestCatFireRate) + "%";
+            fireRateDiv.style.width = ((weapon.FireRate / weapon.HighestCatFireRate)*100) + "%";
 
             magSize.innerText = weapon.MagazineSize;
-            magSizeDiv.style.width = (weapon.MagazineSize / weapon.HighestCatMagazineSize) + "%";
+            magSizeDiv.style.width = ((weapon.MagazineSize / weapon.HighestCatMagazineSize)*100) + "%";
 
             dataContainer.append(weapons);
         })
